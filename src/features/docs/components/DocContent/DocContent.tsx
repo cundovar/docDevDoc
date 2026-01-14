3b263c974b4c5f055255a041f67a75b917076827
Facundo VARAS <102789450+cundovar@users.noreply.github.com>
2026-01-14 13:40:57 +0100

    2

--- src/features/docs/components/DocContent/DocContent.tsx
+++
@@ -1,9 +0,0 @@
-import { renderMarkdown } from '../../lib/markdown'
-
-type DocContentProps = {
-  content: string
-}
-
-export function DocContent({ content }: DocContentProps) {
-  return <div className="space-y-6">{renderMarkdown(content)}</div>
-}
--- src/features/ui/components/Divider/Divider.tsx
+++
@@ -1,8 +0,0 @@
-import { type HTMLAttributes } from 'react'
-import { cn } from '../../lib/cn'
-
-type DividerProps = HTMLAttributes<HTMLHRElement>
-
-export function Divider({ className, ...props }: DividerProps) {
-  return <hr className={cn('border-[var(--doc-line)]', className)} {...props} />
-}
